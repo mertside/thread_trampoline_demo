8c8b44687712f255dcf415375f0f7a80ebb57ad4
Mert Side <mert.side@ttu.edu>
2022-11-03 11:20:26 -0500

    fixing prtyp

--- asm_demo/demo_api_v1.c
+++ asm_demo/demo_api_v1.c
@@ -15,10 +15,10 @@
 void __xbrtime_asm_fence();
 void __xbrtime_asm_quiet_fence();
 
-void __xbrtime_get_u8_seq( uint64_t base_src, uint64_t base_dest, uint32_t pe,
-                           uint32_t nelems, uint32_t stride );
-void __xbrtime_put_u8_seq( uint64_t base_src, uint64_t base_dest, uint32_t pe,
-                           uint32_t nelems, uint32_t stride );
+void __xbrtime_get_u8_seq(uint64_t base_src, uint64_t base_dest,//uint32_t pe,
+                          uint32_t nelems, uint32_t stride );
+void __xbrtime_put_u8_seq(uint64_t base_src, uint64_t base_dest,//uint32_t pe,
+                          uint32_t nelems, uint32_t stride );
 
 
 // ------------------------------------------------------------------ ASSEMBLY
